ebf9902c75247e7c96c4f1588f71b002b02eb798
snrtro <107309506+snrtro@users.noreply.github.com>
2022-11-27 01:30:11 +0500

    Add files via upload

---
+++ bankPercent/bankPercent/Program.cs
@@ -0,0 +1,11 @@
+﻿
+string userInput = Console.ReadLine();
+Console.WriteLine(Calculate(userInput));
+static double Calculate(string userInput)
+{
+    string[] v = userInput.Split(' ');
+    double amount = double.Parse(v[0], System.Globalization.CultureInfo.InvariantCulture);
+    double percent = double.Parse(v[1], System.Globalization.CultureInfo.InvariantCulture);
+    double months = double.Parse(v[2], System.Globalization.CultureInfo.InvariantCulture);
+    return amount*(Math.Pow(1 + percent / 100 / 12, months));
+}
