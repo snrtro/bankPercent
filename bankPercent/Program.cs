2e755b2da0a49dba4cfdd0c582b7af2f132170c9
alexander <107309506+snrtro@users.noreply.github.com>
2023-08-27 15:41:02 +0500

    Update Program.cs

--- bankPercent/Program.cs
+++ bankPercent/Program.cs
@@ -9,3 +9,25 @@ static double Calculate(string userInput)
     double months = double.Parse(v[2], System.Globalization.CultureInfo.InvariantCulture);
     return amount*(Math.Pow(1 + percent / 100 / 12, months));
 }
+static double Calculate(string userInput)
+{
+    string[] v = userInput.Split(' ');
+    double amount = double.Parse(v[0], System.Globalization.CultureInfo.InvariantCulture);
+    double percent = double.Parse(v[1], System.Globalization.CultureInfo.InvariantCulture);
+    double months = double.Parse(v[2], System.Globalization.CultureInfo.InvariantCulture);
+    return amount*(Math.Pow(1 + percent / 100 / 12, months));
+}static double Calculate(string userInput)
+{
+    string[] v = userInput.Split(' ');
+    double amount = double.Parse(v[0], System.Globalization.CultureInfo.InvariantCulture);
+    double percent = double.Parse(v[1], System.Globalization.CultureInfo.InvariantCulture);
+    double months = double.Parse(v[2], System.Globalization.CultureInfo.InvariantCulture);
+    return amount*(Math.Pow(1 + percent / 100 / 12, months));
+}static double Calculate(string userInput)
+{
+    string[] v = userInput.Split(' ');
+    double amount = double.Parse(v[0], System.Globalization.CultureInfo.InvariantCulture);
+    double percent = double.Parse(v[1], System.Globalization.CultureInfo.InvariantCulture);
+    double months = double.Parse(v[2], System.Globalization.CultureInfo.InvariantCulture);
+    return amount*(Math.Pow(1 + percent / 100 / 12, months));
+}
